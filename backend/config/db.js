13f6304a1b057febb085045f96ffcb2e37fa14d9
jonah-11 <jonahjjed@gmail.com>
2024-09-09 14:08:39 +0300

    Updated Files

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise');
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQL_HOST || '127.0.0.1',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '',
+  database: process.env.MYSQL_DATABASE || 'expense_tracker_uno',
+  port: process.env.MYSQL_PORT || 3306
 });
 
 module.exports = pool;
