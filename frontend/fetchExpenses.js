f0d16240bfafad846138e17b82cd78f787348e74
jonah-11 <jonahjjed@gmail.com>
2024-09-10 12:37:25 +0300

    Updated Files

--- frontend/fetchExpenses.js
+++
@@ -1,8 +0,0 @@
-// fetchExpenses.js
-fetch('/api/expenses')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // Check if the data is correct
-    // Handle data display here
-  })
-  .catch(error => console.error('Error fetching data:', error));
